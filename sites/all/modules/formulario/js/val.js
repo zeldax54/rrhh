a1a71fa8e92d0375d6145c6bc2842ca68f57b192
hl <zeldax54@gmail.com>
2017-12-01 00:10:25 -0500

    Cambios en las vistas oportunidades laborables disponibles

--- sites/all/modules/formulario/js/val.js
+++ sites/all/modules/formulario/js/val.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 		$(this).attr('value',id);		
 	});
 
+	
 
    
    
--- sites/all/themes/Porto_sub/template.php
+++ sites/all/themes/Porto_sub/template.php
@@ -8,12 +8,43 @@
  function porto_sub_views_pre_render(&$view) {
     
   if ('oportunidades_laborales_disponibles' == $view->name) {	 
-	  if (!in_array('administrator',$GLOBALS['user']->roles)) {		
+	  if (!in_array('administrator',$GLOBALS['user']->roles)) {
+		//  print_r($view->field['created']);die();		  
 		  unset($view->field['nothing']);	   
+		  unset($view->field['field_sexo']);	   
+		  unset($view->field['field_edad']);	   
+		  unset($view->field['field_fecha_de_inicio']);	   
+		  unset($view->field['created']);	   
+		 
 	  }
 	   
   }  
 }
+
+
+
+
+
+
+
+/*function porto_sub_preprocess_page(&$vars, $hook = null){
+die();
+    if (isset($vars['node'])) {
+        switch ($vars['node']->type) {           
+            /*case 'webform': 
+                drupal_add_js(drupal_get_path('theme', 'THEMENAME').'/js/myscript.js'); // relative path to your script
+                drupal_add_js(drupal_get_path('theme', 'THEMENAME').'/js/myscript-2.js'); // relative path to another script
+                break;*/
+				
+			//	case 'oferta-laboral': 
+			//	die();
+              //  drupal_add_css(drupal_get_path('theme', 'porto_sub').'/css/mycustom.css'); // relative path to your script
+               // drupal_add_js(drupal_get_path('theme', 'THEMENAME').'/js/myscript-2.js'); // relative path to another script
+              //  break;
+      //  }
+   // }
+//}
+
 
 
 
